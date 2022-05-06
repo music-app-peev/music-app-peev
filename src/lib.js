c654fe5dd4f7c68c95d044bbc626a534bdab0d38
Petar Peev <petar_peev83@yahoo.co.uk>
2022-05-07 01:04:02 +0300

    remove node_modules

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,12 @@
-import { html, render } from "../node_modules/lit-html/lit-html.js";
-import page from "../node_modules/page/page.mjs";
+import { html, render } from './lib/lit-html.js';
+import { until } from './lib/directives/until.js';
+import { classMap } from './lib/directives/class-map.js';
+import page from './lib/page.mjs';
 
 export {
     html,
     render,
-    page,
+    until,
+    classMap,
+    page
 };
